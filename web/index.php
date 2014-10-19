f04fdb5ad807e851b831fffb1080ca35b6546de5
michelangelo <dragonbe+github@gmail.com>
2014-10-19 11:22:29 +0200

    Adding Sentry error logging to the project

--- web/index.php
+++ web/index.php
@@ -8,8 +8,11 @@ require_once '../vendor/autoload.php';
 defined('APP_ENV') ||
     define('APP_ENV', isset ($_SERVER['APP_ENV']) ? $_SERVER['APP_ENV'] : 'production');
 
-ini_set('display_errors', 1);
-ini_set('error_display', E_ALL|E_STRICT);
+$client = new Raven_Client('https://cf9f4d8b9ff643eb96b3ac019211d732:173bf2a133144fd0a4f93a4f1a361839@app.getsentry.com/31069');
+$error_handler = new Raven_ErrorHandler($client);
+// Register error handler callbacks
+set_error_handler(array($error_handler, 'handleError'));
+set_exception_handler(array($error_handler, 'handleException'));
 
 $basepath = '/localhost/moodler/web';
 $app = new \Silex\Application();
@@ -38,19 +41,10 @@ $app->get('/mood/{mood}', function ($mood) use ($app) {
     $config = new \Moodler\Config(__DIR__ . '/../config/config.ini', APP_ENV);
     $moodler = new \Moodler\Mood($config);
     $moodler->storeMood($mood);
-    //return $app->json(array ('mood' => $mood),201);
     $path = $app['url_generator']->generate('today');
     return $app->redirect($path, 302);
 })
 ->bind('moodget');
-
-$app->post('/mood', function (Request $request) use ($app) {
-    $mood = $request->request->get('mood');
-
-    /** process mood */
-    return $app->json(array ('mood' => $mood),201);
-})
-->bind('mood');
 
 $app->get('/today', function() use ($app) {
     $config = new \Moodler\Config(__DIR__ . '/../config/config.ini', APP_ENV);
